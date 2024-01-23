bf6131eb007dfa892f80ab3b38cefcc8380e247e
Jazcash <jaspercashmore@gmail.com>
2024-01-23 21:53:56 +0000

    auth wip

--- src/tachyon-client.ts
+++ src/tachyon-client.ts
@@ -1,8 +1,10 @@
+import http from "node:http";
+
+import { generateCodeVerifier, OAuth2Client } from "@badgateway/oauth2-client";
 import chalk from "chalk";
 import { Signal } from "jaz-ts-utils";
 import fetch from "node-fetch";
-import { generators, Issuer } from "openid-client";
-// eslint-disable-next-line no-restricted-imports
+import open from "open";
 import {
     getValidator,
     type RequestData,
@@ -12,10 +14,32 @@ import {
     type ServiceId,
     tachyonMeta,
 } from "tachyon-protocol";
+import { SetOptional } from "type-fest";
 import { ClientOptions, WebSocket } from "ws";
 
 import { SteamSessionTicketResponse } from "@/model/steam-session-ticket.js";
-import { RedirectHandler } from "@/oauth2-redirect-handler.js";
+
+// https://www.rfc-editor.org/rfc/rfc8252
+
+export type LoginOptions = {
+    clientId: string;
+    clientSecret: string;
+    callbackUrl: string;
+    /** An OAuth 2 access token. This should be stored and passed back here for subsequent logins. If the token is undefined or expired, the client will be prompted to authorise. */
+    //token?: TokenSet;
+    /** Specify a method to open the authentication url, defaults to using https://www.npmjs.com/package/open */
+    open: (url: string) => void;
+    /** An abort signal which can be used to terminate the authentication process */
+    abortSignal?: AbortSignal;
+    /** url path that serves as the root for /.well-known/openid-configuration, defaults to \/ */
+    authPath?: string;
+};
+
+const defaultLoginOptions = {
+    open: (url) => open(url),
+    callbackUrl: "http://127.0.0.1:3006/oauth2callback",
+    authPath: "/",
+} satisfies Partial<LoginOptions>;
 
 export interface TachyonClientOptions extends ClientOptions {
     host: string;
@@ -187,81 +211,122 @@ export class TachyonClient {
     }
 
     public async authenticateSteamSessionTicket(sessionToken: string): Promise<SteamSessionTicketResponse> {
-        const url = `http://${this.config.host}:${this.config.port}/steamauth?ticket=${sessionToken}`;
+        const url = `${this.getServerBaseUrl()}/steamauth?ticket=${sessionToken}`;
         const res = await fetch(url);
         const data = await res.json();
         return data as SteamSessionTicketResponse;
     }
 
-    public async register(email: string, password: string, displayName: string) {
-        return fetch(`http://${this.config.host}:${this.config.port}/register`, {
-            method: "post",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-                email,
-                password,
-                displayName,
-            }),
+    public async auth(optionsArg: SetOptional<LoginOptions, keyof typeof defaultLoginOptions>) {
+        const options: LoginOptions = { ...defaultLoginOptions, ...optionsArg };
+
+        const client = new OAuth2Client({
+            server: this.getServerBaseUrl(),
+            clientId: options.clientId,
+            authorizationEndpoint: `${this.getServerBaseUrl()}/oauth/authorize`,
+            tokenEndpoint: `${this.getServerBaseUrl()}/oauth/access_token`,
+        });
+
+        const codeVerifier = await generateCodeVerifier();
+
+        const authUrl = await client.authorizationCode.getAuthorizeUri({
+            redirectUri: options.callbackUrl,
+            codeVerifier,
+        });
+
+        options.open(authUrl);
+
+        // const codeVerifier = await client.generateCodeVerifierAsync();
+
+        // const authUrl = client.generateAuthUrl({
+        //     scope: "openid",
+        //     code_challenge_method: CodeChallengeMethod.S256,
+        //     code_challenge: codeVerifier.codeChallenge,
+        // });
+
+        const callbackRequestUrl = await this.authCallback(options.callbackUrl, options.abortSignal);
+
+        console.log(callbackRequestUrl);
+
+        //const token = await client.getToken()
+
+        //const callbackRequestUrl
+
+        // const issuer = await Issuer.discover(
+        //     `${this.getServerBaseUrl()}${options.authPath}.well-known/oauth-configuration`
+        // );
+
+        // const client = new issuer.Client({
+        //     client_id: options.clientId,
+        //     client_secret: options.clientSecret,
+        //     redirect_uris: [options.callbackUrl],
+        //     response_types: ["code"],
+        // });
+
+        // const code_verifier = generators.codeVerifier();
+        // const url = client.authorizationUrl({
+        //     scope: "openid",
+        //     code_challenge: generators.codeChallenge(code_verifier),
+        //     code_challenge_method: "S256",
+        // });
+
+        // options.open(url);
+
+        // const callbackRequestUrl = await this.authCallback(options.callbackUrl, options.abortSignal);
+
+        // const params = client.callbackParams(callbackRequestUrl);
+        // const tokenSet = await client.callback(options.callbackUrl, params, {
+        //     code_verifier,
+        //     response_type: "code",
+        // });
+
+        // return tokenSet;
+    }
+
+    protected authCallback(callbackUrl: string, abortSignal?: AbortSignal) {
+        const url = new URL(callbackUrl);
+
+        return new Promise<string>((resolve, reject) => {
+            const server = http.createServer();
+            server.addListener("request", (req, res) => {
+                if (req.url) {
+                    const reqUrl = new URL(req.url, `http://${req.headers.host}`);
+
+                    if (!reqUrl.searchParams.get("error")) {
+                        res.writeHead(200, { "Content-Type": "text/plain" });
+                        res.end("Authentication succeeded, you may close this window.");
+                        resolve(req.url);
+                    } else {
+                        res.writeHead(400, { "Content-Type": "text/plain" });
+                        res.end(
+                            `Authentication callback failed: ${
+                                reqUrl.searchParams.get("error_description") || reqUrl.searchParams.get("error")
+                            }`
+                        );
+                        reject("auth failed");
+                    }
+                }
+
+                server.close();
+                server.closeAllConnections();
+            });
+
+            server.listen({
+                host: url.hostname,
+                port: url.port,
+            });
+
+            abortSignal?.addEventListener("abort", () => {
+                server.close();
+                server.closeAllConnections();
+                reject("aborted");
+            });
         });
     }
 
-    private getServerBaseUrl() {
+    protected getServerBaseUrl() {
         const schema = this.config.ssl ? "https" : "http";
         const port = this.config.port ? ":" + this.config.port : "";
         return `${schema}://${this.config.host}${port}`;
-    }
-
-    public async login(signal: AbortSignal, openUrl: (url: string) => void) {
-        const issuer = await Issuer.discover(`${this.getServerBaseUrl()}/oidc`);
-        const rh = new RedirectHandler(signal);
-        try {
-            const redirectUri = await rh.getRedirectUrl();
-            //const redirectUri = "https://oidcdebugger.com/debug";
-            //console.log(redirectUrl);
-
-            const client = new issuer.Client({
-                client_id: "BAR Lobby",
-                client_secret: "fish",
-                redirect_uris: [redirectUri],
-                response_types: ["code"],
-            });
-
-            const code_verifier = generators.codeVerifier();
-            const url = client.authorizationUrl({
-                scope: "openid lobby",
-                code_challenge: generators.codeChallenge(code_verifier),
-                code_challenge_method: "S256",
-            });
-            openUrl(url);
-
-            const callbackUrl = await rh.waitForCallback();
-            const params = client.callbackParams(callbackUrl);
-            const tokenSet = await client.callback(redirectUri, params, {
-                code_verifier,
-                response_type: "code",
-            });
-
-            //  TODO: Actually do something useful with the token.
-            console.log(tokenSet);
-
-            return tokenSet;
-        } finally {
-            console.log("closed thing");
-            rh.close();
-        }
-    }
-
-    public async thing(token: string) {
-        const body = new URLSearchParams();
-        body.append("token", token);
-
-        return fetch(`${this.getServerBaseUrl()}/oidc/token/introspection`, {
-            method: "post",
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded",
-                Authorization: "Basic " + "idk",
-            },
-            body,
-        });
     }
 }
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
+    target: "node18",
     entry: ["src/index.ts"],
     bundle: true,
     clean: true,
     dts: true,
     shims: true,
     sourcemap: true,
-    format: ["cjs", "esm"],
+    format: ["esm"],
 });
